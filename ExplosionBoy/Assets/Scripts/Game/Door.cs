a16e2c092130130233f0804b0d3b50d7d6688556
pancreasio <julianholisu@gmail.com>
2019-05-20 15:40:09 -0300

    working win condition

--- ExplosionBoy/Assets/Scripts/Game/Door.cs
+++ ExplosionBoy/Assets/Scripts/Game/Door.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+    private Material material;
+
+    private void Start()
+    {
+        material = transform.GetComponent<Renderer>().material;
+    }
+    private void Update()
+    {
+        if (Enemy.activeEnemies <= 0)
+        {
+            material.color = Color.yellow;
+        }
+        else
+        {
+            material.color = Color.grey;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "Player" && Enemy.activeEnemies <= 0)
